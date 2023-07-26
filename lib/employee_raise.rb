994e95b87660360b34e6da28b0ef238dfc23152f
Melony Franchini <meltravelz@gmail.com>
2023-07-26 15:33:02 -0600

    Complete first attempt

--- lib/employee_raise.rb
+++ lib/employee_raise.rb
@@ -1,2 +1,28 @@
+require 'json'
+
 class EmployeeRaise
+
+  def give_raise
+    #read, parse, sort data:
+    data_hash = File.read("data/employee_information.json")
+    parsed_data = JSON.parse(data_hash)
+    emp_array = parsed_data["data"]["employees"]
+
+    #make raise
+    new_salary_hash = Hash.new
+    emp_array.each do |emp|
+      current_salary = emp["salary"].values.last
+
+      if current_salary >= 70000
+        perc_raise = 0.05
+      else
+        perc_raise = 0.1
+      end
+
+      new_salary = current_salary + (current_salary * perc_raise)
+      new_salary_hash[emp["name"]] = new_salary
+    end
+
+    new_salary_hash
+  end
 end
